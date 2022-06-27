85eb0dc749cb9a2632ecdad752ed0111944cacfb
Behruz <behruzparpiyev20@gmail.com>
2022-06-27 19:00:29 +0500

    ...

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+# from main.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main.urls'))
+    path('', include('main.urls')),
+    # path('', HomeView.as_view(), name='home')
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-
+from .views import CreateBookview, HomeView, UpdateBookView
 
 urlpatterns = [
-    
+    path('home/', HomeView.as_view(), name='home'),
+    path('create/', CreateBookview.as_view(), name='create'),
+    path('update/<int:pk>/', UpdateBookView.as_view(), name='update')
 ]
